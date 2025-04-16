3f4d95fdd92a0c1ea85e894d756bd07d364d63fd
TeutonicTerror <136272771+TeutonicTerrror@users.noreply.github.com>
2025-04-16 14:43:19 -0500

    Some IFrame Fixes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const playButton = document.getElementById('playButton');
-  const ytPlayer = document.getElementById('ytplayer');
+let ytPlayer;
 
-  playButton.addEventListener('click', () => {
-    ytPlayer.src += '&autoplay=1';
-    playButton.style.display = 'none';
+window.onYouTubeIframeAPIReady = function() {
+  ytPlayer = new YT.Player('ytplayer', {
+    height: '0',
+    width: '0',
+    videoId: '41O_MydqxTU',
+    playerVars: {
+      autoplay: 0,
+      controls: 0,
+      disablekb: 1,
+      modestbranding: 1,
+      rel: 0
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
   });
-});
+};
+
+function onPlayerReady(event) {
+  document.getElementById('playButton').addEventListener('click', () => {
+    event.target.playVideo();
+    event.target.setVolume(100);
+    document.getElementById('playButton').style.display = 'none';
+  });
+}
+
+function onPlayerStateChange(event) {
+  if (event.data === YT.PlayerState.ENDED) {
+    document.getElementById('playButton').style.display = 'inline-block';
+  }
+}
